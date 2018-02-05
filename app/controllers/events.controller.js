c22711f788f06dc3af0e855425e97cb2b2ef1ad0
Alexandra Swart <answart@sbcglobal.net>
2018-02-05 04:10:06 -0800

    Remove cb from url validity check

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -26,8 +26,9 @@ function show404(req, res) {
 module.exports = {
   show404: show404,
   showUrl: (req, res) => {
-    var paramUrl = req.params['0'];
-    var isAppsShortenedUrl = urlHelper.isAppUrl(paramUrl);
+    var url = req.params['0'];
+    var isAppsShortenedUrl = urlHelper.isAppUrl(url);
+    var isValidUrl = urlHelper.isValidUrl(url);
 
     function returnHome(err) {
       req.flash('errors', err);
@@ -38,34 +39,32 @@ module.exports = {
     }
 
     if (isAppsShortenedUrl) {
-      UrlList.findOne({ short_url: paramUrl }, (err, urlList) => {
+      UrlList.findOne({ short_url: url }, (err, urlList) => {
         if (err) { throw err };
         if (urlList) {
           res.redirect(urlList.original_url);
         } else {
-          returnHome(`'${paramUrl}' is not a valid shortened url. Try again.`)
+          returnHome(`'${url}' is not a valid shortened url. Try again.`)
         }
       })
     } else {
-      urlHelper.isValidUrl(paramUrl, (err, url) => {
-        if (err) {
-          returnHome(err);
-        } else {
-          UrlList.findOne({ original_url: url }, (err, urlList) => {
-            if (err) { throw err };
-            if (urlList) {
+      if (!isValidUrl) {
+        returnHome(`'${url}' is not a valid url. Try again.`)
+      } else {
+        UrlList.findOne({ original_url: url }, (err, urlList) => {
+          if (err) { throw err };
+          if (urlList) {
 
-              res.json(urlList.toJSON());
-            } else {
-              createShortUrl(url, (err, doc) => {
-                if (err) { throw err };
+            res.json(urlList.toJSON());
+          } else {
+            createShortUrl(url, (err, doc) => {
+              if (err) { throw err };
 
-                res.json(doc.toJSON());
-              })
-            }
-          })
-        }
-      })
+              res.json(doc.toJSON());
+            })
+          }
+        })
+      }
     }
   }
 
