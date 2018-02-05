f5012036cd37c5777989139cd6eae97f718afabf
Alexandra Swart <answart@sbcglobal.net>
2018-02-05 03:18:06 -0800

    Add connect-mongo package

--- server.js
+++ server.js
@@ -9,18 +9,34 @@ const express    = require('express'),
   mongoose       = require('mongoose'),
   bodyParser     = require('body-parser'),
   session        = require('express-session'),
+  MongoStore     = require('connect-mongo')(session),
   cookieParser   = require('cookie-parser'),
   flash          = require('connect-flash');
 
 
+mongoose.connect(process.env.DB_URI);
+mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+  }
+);
+
+app.set('trust proxy', 1);
 // Set sessions and cookie parser
 app.use(cookieParser());
 app.use(session({
   secret: 'my-super-secret',
   cookie: { maxAge: 6000 },
   resave: false, // forces session to be saved to store
-  saveUninitialized: false // dont save unmodified
+  saveUninitialized: false, // dont save unmodified
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
+app.use(function(req,res,next){
+  if(!req.session){
+    return next(new Error('Oh no! No session found.')) //handle error
+  }
+  next();
+});
 app.use(flash());
 
 // Set public directories
@@ -29,14 +45,6 @@ app.use('/public', express.static(__dirname + '/public'));
 // Set ejs as templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-
-// Connect to db
-mongoose.connect(process.env.DB_URI);
-mongoose.connection.on('error', function(err) {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-  }
-);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
