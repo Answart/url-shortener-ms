f05a4b4406855a8194990ccd38e4837a7c636e96
Alexandra Swart <answart@sbcglobal.net>
2019-02-06 01:48:57 -0800

    Routes watch for create/get shortUrl routes

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/',                   MainController.showHome);
 router.get('/new/*',              ShortUrlController.showUrl);
+router.get('/s/*',                ShortUrlController.showUrl);
 router.get('/*',                  MainController.show404);
 
 module.exports = router;
