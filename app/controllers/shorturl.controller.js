38ce0fb90481492607346bb040e237dec2678806
Alexandra Swart <answart@sbcglobal.net>
2019-02-06 01:51:21 -0800

    Split shorturl.controller showUrl into createShortUrl/getShortUrl

--- app/controllers/shorturl.controller.js
+++ app/controllers/shorturl.controller.js
@@ -2,54 +2,82 @@ const { ShortUrl } = require('../models');
 const { isValidShortUrl, isValidUrl } = require('../utils');
 
 
-function showUrl(req, res) {
-  var url = req.params['0'];
-  var isAppsShortenedUrl = isValidShortUrl(url);
-  var validUrl = isValidUrl(url);
+function createShortUrl(req, res) {
+  const original_url = req.params['0'];
+  const validUrl = isValidUrl(original_url);
 
-  function returnHome(err) {
-    req.flash('errors', err);
+  function returnHome(message) {
+    req.flash('errors', message);
 
-    res.render('pages/home', {
+    console.error(message);
+    res.statusMessage = message;
+    res.status(400).render('pages/home', {
       errors: req.flash('errors')
     });
   }
 
-  if (isAppsShortenedUrl) {
-    ShortUrl.findOne({ short_url: url }, (err, urlDoc) => {
-      if (err) { throw err };
-      if (urlDoc) {
-        res.redirect(urlDoc.original_url);
-      } else {
-        returnHome(`'${url}' is not a valid shortened url. Try again.`)
-      }
-    })
-  } else {
-    if (!validUrl) {
-      returnHome(`'${url}' is not a valid url. Try again.`)
+  if (!validUrl) {
+    returnHome(`Given url '${original_url}' is not a valid url to shorten.`);
+  }
+
+  // user give a previously created url?
+  ShortUrl.findOne({ original_url }, (err, foundUrl) => {
+    if (err) {
+      returnHome(`An error occured while looking for the shortUrl of '${original_url}'. Try again. Error: ${err}`);
+    } else if (!!foundUrl) {
+      console.log(`A shortUrl already exists for '${original_url}' as '${foundUrl.short_url}'.`)
+      res.status(200).json(foundUrl.toJSON());
     } else {
-      ShortUrl.findOne({ original_url: url }, (err, urlDoc) => {
-        if (err) { throw err };
-        if (urlDoc) {
+      const url = new ShortUrl({ original_url });
 
-          res.json(urlDoc.toJSON());
+      url.save((err, createdUrl) => {
+        if (err) {
+          returnHome(`An error occured while saving new shortUrl from '${original_url}'. Error: ${err}`);
+        } else if (!createdUrl) {
+          returnHome(`An error occured while creating shortUrl from '${original_url}'.`);
         } else {
-          var shortUrl = new ShortUrl({
-            original_url: url
-          })
-
-          shortUrl.save((err, urlDoc) => {
-            if (err) { throw err };
-
-            res.json(doc.toJSON());
-          })
+          console.log(`Created shortUrl '${createdUrl.short_url}' from shortUrl '${original_url}'.`)
+          res.status(200).json(createdUrl.toJSON());
         }
       })
     }
+  })
+};
+
+function getShortUrl(req, res) {
+  const short_url = req.params['0'];
+  const fullPath = `${process.env.PUBLIC_URL}/s/${short_url}`;
+  const validShortUrl = isValidShortUrl(short_url);
+
+  function returnHome(message) {
+    req.flash('errors', message);
+
+    console.error(message);
+    res.statusMessage = message;
+    res.status(400).render('pages/home', {
+      errors: req.flash('errors')
+    });
   }
+
+  if (!validShortUrl) {
+    returnHome(`Given url '${short_url}' was not created here. Unknown route`);
+  }
+
+  // user give a shortUrl?
+  ShortUrl.findOne({ short_url: fullPath }, (err, foundUrl) => {
+    if (err) {
+      returnHome(`An error occured while looking for the original url of shortUrl '${short_url}'. Try again. Error: ${err}`);
+    } else if (!foundUrl || !foundUrl.original_url) {
+      returnHome(`No url associated with the shortUrl '${fullPath}'.`);
+    } else {
+      console.log(`Found url '${foundUrl.original_url}' from shortUrl '${short_url}'. Redirecting user.`)
+      res.status(200).redirect(foundUrl.original_url);
+    }
+  })
 };
 
 
 module.exports = {
-  showUrl
+  createShortUrl,
+  getShortUrl
 };
--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 
 router.get('/',                   MainController.showHome);
-router.get('/new/*',              ShortUrlController.showUrl);
-router.get('/s/*',                ShortUrlController.showUrl);
+router.get('/new/*',              ShortUrlController.createShortUrl);
+router.get('/s/*',                ShortUrlController.getShortUrl);
 router.get('/*',                  MainController.show404);
+
 
 module.exports = router;
