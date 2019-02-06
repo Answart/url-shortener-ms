00f53ddf65165309553c0d6a80fc745e7ff879f3
Alexandra Swart <answart@sbcglobal.net>
2019-02-06 01:47:20 -0800

    Refactor/cleanup shorturl.controller

--- app/controllers/shorturl.controller.js
+++ app/controllers/shorturl.controller.js
@@ -2,61 +2,54 @@ const { ShortUrl } = require('../models');
 const { isValidShortUrl, isValidUrl } = require('../utils');
 
 
-function createShortUrl(original_url, cb) {
-  var shortUrl = new ShortUrl({
-    original_url
-  })
+function showUrl(req, res) {
+  var url = req.params['0'];
+  var isAppsShortenedUrl = isValidShortUrl(url);
+  var validUrl = isValidUrl(url);
 
-  shortUrl.save((err, urlDoc) => {
-    if (err) { throw err };
+  function returnHome(err) {
+    req.flash('errors', err);
 
-    cb(null, urlDoc);
-  })
-}
+    res.render('pages/home', {
+      errors: req.flash('errors')
+    });
+  }
+
+  if (isAppsShortenedUrl) {
+    ShortUrl.findOne({ short_url: url }, (err, urlDoc) => {
+      if (err) { throw err };
+      if (urlDoc) {
+        res.redirect(urlDoc.original_url);
+      } else {
+        returnHome(`'${url}' is not a valid shortened url. Try again.`)
+      }
+    })
+  } else {
+    if (!validUrl) {
+      returnHome(`'${url}' is not a valid url. Try again.`)
+    } else {
+      ShortUrl.findOne({ original_url: url }, (err, urlDoc) => {
+        if (err) { throw err };
+        if (urlDoc) {
+
+          res.json(urlDoc.toJSON());
+        } else {
+          var shortUrl = new ShortUrl({
+            original_url: url
+          })
+
+          shortUrl.save((err, urlDoc) => {
+            if (err) { throw err };
+
+            res.json(doc.toJSON());
+          })
+        }
+      })
+    }
+  }
+};
 
 
 module.exports = {
-  showUrl: (req, res) => {
-    var url = req.params['0'];
-    var isAppsShortenedUrl = isValidShortUrl(url);
-    var validUrl = isValidUrl(url);
-
-    function returnHome(err) {
-      req.flash('errors', err);
-
-      res.render('pages/home', {
-        errors: req.flash('errors')
-      });
-    }
-
-    if (isAppsShortenedUrl) {
-      ShortUrl.findOne({ short_url: url }, (err, urlDoc) => {
-        if (err) { throw err };
-        if (urlDoc) {
-          res.redirect(urlDoc.original_url);
-        } else {
-          returnHome(`'${url}' is not a valid shortened url. Try again.`)
-        }
-      })
-    } else {
-      if (!validUrl) {
-        returnHome(`'${url}' is not a valid url. Try again.`)
-      } else {
-        ShortUrl.findOne({ original_url: url }, (err, urlDoc) => {
-          if (err) { throw err };
-          if (urlDoc) {
-
-            res.json(urlDoc.toJSON());
-          } else {
-            createShortUrl(url, (err, doc) => {
-              if (err) { throw err };
-
-              res.json(doc.toJSON());
-            })
-          }
-        })
-      }
-    }
-  }
-
+  showUrl
 };
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,9 @@ const { MainController, ShortUrlController } = require('./controllers');
 const express = require('express');
 const router = express.Router();
 
-router.get('/',      MainController.showHome);
-router.get('/new/*', ShortUrlController.showUrl);
-router.get('*',      MainController.show404);
 
+router.get('/',                   MainController.showHome);
+router.get('/new/*',              ShortUrlController.showUrl);
+router.get('/*',                  MainController.show404);
 
 module.exports = router;
