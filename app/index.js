f8b3974cb3ffc88b3abf67a7967b226713368950
Alexandra Swart <answart@sbcglobal.net>
2019-02-06 00:47:24 -0800

    Move app config from root file to app/index

---
+++ app/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config({ path: '../.env' });
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const secret = process.env.SECRET || '';
+
+
+module.exports = (store) => {
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || 8000;
+  app.set('host', host);
+  app.set('port', port);
+  app.set('trust proxy', 1);
+  app.set('strict routing', true);
+
+  // Set sessions and cookie parser
+  app.use(cookieParser());
+  app.use(session({
+    secret,
+    cookie: { maxAge: 6000 },
+    resave: false, // forces session to be saved to store
+    saveUninitialized: false, // dont save unmodified
+    store
+  }));
+  app.use(function(req,res,next){
+    if(!req.session){
+      return next(new Error('Oh no! No session found.')) //handle error
+    }
+    next();
+  });
+  app.use(flash());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+
+  return app;
+};
--- server.js
+++ server.js
@@ -2,37 +2,13 @@
 
 require('dotenv').config();
 
-const express    = require('express'),
-  app            = express(),
-  port           = process.env.PORT || 8000,
-  expressLayouts = require('express-ejs-layouts'),
-  bodyParser     = require('body-parser'),
-  session        = require('express-session'),
-  cookieParser   = require('cookie-parser'),
-  flash          = require('connect-flash');
-
-
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./app/db')(process.env.MONGODB_URI);
 const store = require('./app/store')(db.connection);
 const routes = require('./app/routes');
+const app = require('./app')(store);
 
-app.set('trust proxy', 1);
-// Set sessions and cookie parser
-app.use(cookieParser());
-app.use(session({
-  secret: 'my-super-secret',
-  cookie: { maxAge: 6000 },
-  resave: false, // forces session to be saved to store
-  saveUninitialized: false, // dont save unmodified
-  store
-}));
-app.use(function(req,res,next){
-  if(!req.session){
-    return next(new Error('Oh no! No session found.')) //handle error
-  }
-  next();
-});
-app.use(flash());
 
 // Set public directories
 app.use('/public', express.static(__dirname + '/public'));
@@ -41,13 +17,9 @@ app.use('/public', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Set routes
 app.use(routes);
 
 
-app.listen(port, function() {
-  console.log('Server running on port ' + port);
+app.listen(app.get('port'), app.get('host'), error => {
+  console.log('Server running on port ' + app.get('port'));
 });
