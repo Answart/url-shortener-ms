04a50dfc52f39229d3029fad1ff28b5aa52b8cec
Alexandra Swart <answart@sbcglobal.net>
2019-02-06 01:31:32 -0800

    Create utils/isValidShortUrl fn

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,8 +1,10 @@
 const createRandomString = require('./createRandomString');
+const isValidShortUrl = require('./isValidShortUrl');
 const isValidUrl = require('./isValidUrl');
 
 
 module.exports = {
   createRandomString,
+  isValidShortUrl,
   isValidUrl
 };
---
+++ app/utils/isValidShortUrl.js
@@ -0,0 +1,6 @@
+
+module.exports = (urlStr) => {
+  console.log('isAppUrl?', urlStr)
+  return true;
+  // return urlStr.includes('shrt');
+}
