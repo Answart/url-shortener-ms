46ddb1adcd0a5e31913abbfb9f2d8834ee129e73
Alexandra Swart <answart@sbcglobal.net>
2019-02-06 03:53:45 -0800

    Add .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  rules: {
+    "no-extra-boolean-cast": "off",
+    "import/newline-after-import": "off",
+    "camelcase": "off",
+    "space-before-function-paren": "off",
+    "no-console": "warn",
+    "no-multi-spaces": "warn",
+    "no-unused-vars": "warn",
+    "no-underscore-dangle": "warn",
+    "key-spacing": "warn",
+    "object-shorthand": "warn",
+  },
+};
